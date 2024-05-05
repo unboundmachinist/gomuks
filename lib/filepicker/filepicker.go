500a4275161a06d69220fc9b5e4aef29dac5ebf5
Unbound Machinist <168180907+unboundmachinist@users.noreply.github.com>
2024-05-05 02:04:42 +0100

    Add support for additional filepickers

--- lib/filepicker/filepicker.go
+++ lib/filepicker/filepicker.go
@@ -20,21 +20,52 @@ import (
 	"bytes"
 	"errors"
 	"os/exec"
+	"os"
 	"strings"
 )
 
-var zenity string
+var exists = []string{}
+var cmdArgs = map[string]string{
+	"zenity":	"--file-selection",
+	"yad":		"--file",
+	"kdialog":	"--getopenfilename",
+}
 
 func init() {
-	zenity, _ = exec.LookPath("zenity")
+	var path string
+	var dialogs = desktopDialogHierarchy()
+	for _, prog := range dialogs {
+		path, _ = exec.LookPath(prog)
+		if len(path) > 0 {
+			exists = append(exists, path)
+		}
+	}
 }
 
 func IsSupported() bool {
-	return len(zenity) > 0
+	return len(exists) > 0
 }
 
+func desktopDialogHierarchy() ([3]string) {
+	var XDG_CURRENT_DESKTOP = strings.ToLower(os.Getenv("XDG_CURRENT_DESKTOP"))
+	var DESKTOP_SESSION = strings.ToLower(os.Getenv("DESKTOP_SESSION"))
+
+	if XDG_CURRENT_DESKTOP == "kde" || DESKTOP_SESSION == "kde" {
+		return [3]string{"kdialog", "yad", "zenity"}
+	} else if XDG_CURRENT_DESKTOP == "lxqt" || DESKTOP_SESSION == "lxqt" {
+		return [3]string{"kdialog", "yad", "zenity"}
+	} else if XDG_CURRENT_DESKTOP == "gnome" || DESKTOP_SESSION == "gnome" {
+		return [3]string{"zenity", "yad", "kdialog"}
+	} else {
+		return [3]string{"yad", "zenity", "kdialog"}
+	}
+}
+
+
+
 func Open() (string, error) {
-	cmd := exec.Command(zenity, "--file-selection")
+	var dialog []string = strings.Split(exists[0], "/")
+	cmd := exec.Command(exists[0], cmdArgs[dialog[len(dialog)-1]])
 	var output bytes.Buffer
 	cmd.Stdout = &output
 	err := cmd.Run()
